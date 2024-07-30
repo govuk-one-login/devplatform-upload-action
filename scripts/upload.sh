1bcb8a25ec0ee3486d4c394d0ed19be877a463f2
tobias.saunders <tobias.saunders@digital.cabinet-office.gov.uk>
2024-07-30 11:52:02 +0100

    echo out cf-template.yaml for debugging

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -34,15 +34,17 @@ else
     SKIP_CANARY_DEPLOYMENT=0
 fi
 
-echo "Writing Lambda provenance"
+echo "Processed Template:"
 cat cf-template.yaml
-if  yq '.Resources.* | select(has("Type") and .Type == "AWS::Serverless::Function") | .Properties.CodeUri' cf-template.yaml; then
+if  [[ $(yq '.Resources.* | select(has("Type") and .Type == "AWS::Serverless::Function") | .Properties.CodeUri' cf-template.yaml) ]]; then
+  echo "Writing Lambda provenance"
   yq '.Resources.* | select(has("Type") and .Type == "AWS::Serverless::Function") | .Properties.CodeUri' cf-template.yaml \
     | xargs -L1 -I{} aws s3 cp "{}" "{}" --metadata "repository=$GITHUB_REPOSITORY,commitsha=$GITHUB_SHA,committag=$GIT_TAG,commitmessage=$COMMIT_MSG,commitauthor='$GITHUB_ACTOR',release=$VERSION_NUMBER"
-fi
-echo "Writing Lambda Layer provenance"
-yq '.Resources.* | select(has("Type") and .Type == "AWS::Serverless::LayerVersion") | .Properties.ContentUri' cf-template.yaml \
+
+  echo "Writing Lambda Layer provenance"
+  yq '.Resources.* | select(has("Type") and .Type == "AWS::Serverless::LayerVersion") | .Properties.ContentUri' cf-template.yaml \
     | xargs -L1 -I{} aws s3 cp "{}" "{}" --metadata "repository=$GITHUB_REPOSITORY,commitsha=$GITHUB_SHA,committag=$GIT_TAG,commitmessage=$COMMIT_MSG,commitauthor='$GITHUB_ACTOR',release=$VERSION_NUMBER"
+fi
 
 echo "Zipping the CloudFormation template"
 zip template.zip cf-template.yaml
