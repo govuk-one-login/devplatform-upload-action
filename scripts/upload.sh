c8008965e7a1fa1b620c3f9cdc03d93af9ade918
tobias.saunders <tobias.saunders@digital.cabinet-office.gov.uk>
2024-07-30 11:20:42 +0100

    echo out cf-template.yaml for debugging

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -35,6 +35,7 @@ else
 fi
 
 echo "Writing Lambda provenance"
+cat cf-template.yaml
 yq '.Resources.* | select(has("Type") and .Type == "AWS::Serverless::Function") | .Properties.CodeUri' cf-template.yaml \
     | xargs -L1 -I{} aws s3 cp "{}" "{}" --metadata "repository=$GITHUB_REPOSITORY,commitsha=$GITHUB_SHA,committag=$GIT_TAG,commitmessage=$COMMIT_MSG,commitauthor='$GITHUB_ACTOR',release=$VERSION_NUMBER"
 echo "Writing Lambda Layer provenance"
