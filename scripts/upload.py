93bdb2b93cdaaea8c954a6c145b7b8465cbac9f2
Beca Galliano <beca.galliano@digital.cabinet-office.gov.uk>
2024-11-04 18:26:36 +0000

    Update metadata & join signing profile result

--- scripts/upload.py
+++ scripts/upload.py
@@ -17,6 +17,7 @@ github_actor = os.environ['GITHUB_ACTOR']
 version_number = os.environ['VERSION_NUMBER']
 merge_time = os.environ['MERGE_TIME']
 skip_canary = os.environ['SKIP_CANARY_DEPLOYMENT']
+cf_template = 'cf-template.yaml'
 
 # signing_profile_name = "SigningProfile_2KY7QMWu5CUY"
 # template_file_name = "/Users/bgalliano/Projects/GDS/di-devplatform/devplatform-upload-action/sam-app2/.aws-sam/build/json-template.yaml"
@@ -29,8 +30,9 @@ skip_canary = os.environ['SKIP_CANARY_DEPLOYMENT']
 # version_number = "v123"
 # merge_time = 0
 # skip_canary = 0
+# cf_template = "/Users/bgalliano/Projects/GDS/di-devplatform/devplatform-upload-action/sam-app2/.aws-sam/build/cf-template.yaml"
 
-def signing_profiles(template_file_name):
+def signing_profiles_list(template_file_name):
   with open (template_file_name) as templateFile:
     app_template = Template.from_string(templateFile.read())
     print("Parsing resources to be signed")
@@ -46,22 +48,25 @@ def signing_profiles(template_file_name):
     for resource in resources:
       signing_profiles += f'{resource}={signing_profile_name} '
 
-    return signing_profiles
+    print("signing profiles:", ''.join(signing_profiles))
+
+  return ''.join(signing_profiles)
+
+signing_profiles = signing_profiles_list(template_file_name)
 
 
 def sign_resources(template_file_name, signing_profiles):
   print("Signing resources with sam package")
   if signing_profiles:
-    os.system(f'sam package --s3-bucket="$ARTIFACT_BUCKET" --template-file={template_file_name} --output-template-file=cf-template.yaml --use-json --signing-profiles {signing_profiles}')
+    os.system(f'sam package --s3-bucket=a{artifact_bucket} --template-file={template_file_name} --output-template-file=cf-template.yaml --use-json --signing-profiles {signing_profiles}')
   else:
-    os.system(f'sam package --s3-bucket="$ARTIFACT_BUCKET" --template-file={template_file_name} --output-template-file=cf-template.yaml --use-json')
+    os.system(f'sam package --s3-bucket={artifact_bucket} --template-file={template_file_name} --output-template-file=cf-template.yaml --use-json')
   return
-cf_template="cf-template.yaml"
 
 def lambda_provenance(cf_template):
   print("Writing Lambda provenance")
   with open (cf_template) as cftemplateFile:
-    metadata = [f"repository=' '.join({repository})",
+    metadata = [f'repository={repository}',
                 f'commitsha={commit_sha}',
                 f'committag={commit_tag}',
                 f'commitmessage={commit_message}',
@@ -75,9 +80,9 @@ def lambda_provenance(cf_template):
     for resource, configuration in functions.items():
       print("lambda provenance")
       CodeUri = configuration['Properties']['CodeUri']
-      print("codeuri", CodeUri)
+      print("codeuri:", CodeUri)
       os.system(f'aws s3 cp {CodeUri} {CodeUri} --metadata {metadata}')
-      print("metadata", metadata)
+      print("metadata:", metadata)
 
     print ("Writing Lambda Layer provenance")
     for resource, configuration in layers.items():
@@ -89,7 +94,7 @@ def lambda_provenance(cf_template):
 
 def upload_artifact():
   print("Zipping the CloudFormation template")
-  os.system('zip template.zip cf-template.yaml')
+  os.system(f'zip template.zip {cf_template}')
   print("Uploading zipped CloudFormation artifact to S3")
   metadata = [f'repository={repository}',
               f'commitsha={commit_sha}',
@@ -103,7 +108,7 @@ def upload_artifact():
   metadata = ','.join(metadata)
   os.system(f'aws s3 cp template.zip "s3://{artifact_bucket}/template.zip" --metadata {metadata}')
 
-signing_profiles(template_file_name)
+signing_profiles_list(template_file_name)
 sign_resources(template_file_name, signing_profiles)
 lambda_provenance(cf_template)
 upload_artifact()
