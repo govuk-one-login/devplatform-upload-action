3dd99d65ccf2377a713b120986ce71e4ba731c2d
Karol Gancarz <karol.gancarz@digital.cabinet-office.gov.uk>
2025-02-25 23:01:54 +0000

    PSREDEV-1910: Add a dev script to invoke the upload script locally
    Make it easy to run and test the script locally when making changes.
    
      - Use the demo sam-app2 template by default
      - Build the demo sam-app2 if it's not been built yet
      - Create the default artifact bucket for local testing if it doesn't already exist
      - Get the signing profile name using the AWS CLI

---
+++ upload.local.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+: "${SIGN_CODE:=false}"
+: "${GITHUB_ACTOR:=$(whoami)}"
+: "${GITHUB_REPOSITORY:=upload-action-local}"
+
+default_bucket=upload-action-test-local
+aws sts get-caller-identity > /dev/null || exit
+
+if ! [[ ${TEMPLATE_FILE:-} ]]; then
+  demo_app="$(dirname "${BASH_SOURCE[0]}")/../devplatform-demo-sam-app/sam-app2"
+  export TEMPLATE_FILE="$demo_app/.aws-sam/build/template.yaml"
+
+  if ! [[ -f $TEMPLATE_FILE ]]; then
+    echo "» Building demo sam app"
+    pushd "$demo_app" > /dev/null
+    sam build --cached --parallel
+    popd > /dev/null
+  fi
+fi
+
+if ! [[ ${ARTIFACT_BUCKET:-} ]]; then
+  export ARTIFACT_BUCKET=$default_bucket
+
+  if ! aws s3 ls $ARTIFACT_BUCKET &> /dev/null; then
+    aws s3 mb "s3://$ARTIFACT_BUCKET"
+    aws s3api put-bucket-versioning --bucket $ARTIFACT_BUCKET --versioning-configuration Status=Enabled
+  fi
+fi
+
+if $SIGN_CODE; then
+  SIGNING_PROFILE=$(aws signer list-signing-profiles --query 'profiles[0].profileName' --output text)
+  echo "ℹ Using signing profile $SIGNING_PROFILE"
+  export SIGNING_PROFILE
+fi
+
+echo "ℹ Using template $TEMPLATE_FILE"
+echo "ℹ Using bucket $ARTIFACT_BUCKET"
+
+export GITHUB_ACTOR GITHUB_REPOSITORY
+scripts/upload.sh
